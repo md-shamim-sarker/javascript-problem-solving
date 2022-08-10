543a3e10992a0d536d91ddd3c826635ee590ac14
shamimspro <shamimspro@gmail.com>
2022-08-11 00:47:05 +0600

    minor change in code

--- assignment4.js
+++ assignment4.js
@@ -3,8 +3,9 @@ function radianToDegree(radian) {
     if (typeof radian !== 'number') {
         return 'Please enter a valid number.';
     }
-    return ((radian * 180) / Math.PI).toFixed(2);
+    return Number(((radian * 180) / Math.PI).toFixed(2));
 }
+radianToDegree(10);
 
 // Problem-2
 function isJavaScriptFile(fileName) {
@@ -39,14 +40,20 @@ function isBestFriend(object1, object2) {
     if (typeof object1 !== 'object' || typeof object2 !== 'object') {
         return 'Please enter a valid object.';
     }
-    return object1.name === object2.friend && object1.friend === object2.name;
+    return (
+        object1.name.toLowerCase === object2.friend.toLowerCase &&
+        object1.friend.toLowerCase === object2.name.toLowerCase
+    );
 }
 
-console.log(radianToDegree('10'));
-// console.log(isJavaScriptFile(10));
-// console.log(oilPrice(30, 20, '10'));
-// console.log(publicBusFare(365));
+// console.log(radianToDegree(10));
+// console.log(isJavaScriptFile('image.jpg.js'));
+// console.log(oilPrice(0, 2, 3));
+console.log(publicBusFare(15));
 
-// const obj1 = { name: 'abul', friend: 'alex' };
-// const obj2 = { name: 'babul', friend: 'abul' };
-// console.log(isBestFriend(obj1, true));
+const obj1 = { name: 'abul', friend: 'alex' };
+const obj2 = { name: 'alex', friend: 'abul' };
+// console.log(isBestFriend(obj1, obj2));
+
+// let str = 'Shamim';
+// console.log(str.toLowerCase());
